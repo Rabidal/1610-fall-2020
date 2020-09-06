d417cdfe07b84d789c9f74a119ee092baf99bc25
Racheal <10573008@my.uvu.edu>
2020-09-06 14:37:15 -0600

    editing mover script with cube

--- Cyborg shrimp/Assets/Scripts/MoverScript.cs
+++ Cyborg shrimp/Assets/Scripts/MoverScript.cs
@@ -7,12 +7,12 @@ public class MoverScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(message:"All my jokes are cries for help");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      transform.Translate(0.1f,0,0);
     }
 }
