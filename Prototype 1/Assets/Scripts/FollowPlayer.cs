8e730b83b69fafb111cc261962ea08846579c0f3
Racheal <10573008@my.uvu.edu>
2020-09-21 19:57:32 -0600

    und panzer

---
+++ Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 _offset = new Vector3(0,6,-7);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //off set the camera behind the player by adding to the players position
+        transform.position = player.transform.position + _offset;
+    }
+}
--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float speed = 5.0f;
+    private float turnSpeed=25.0f;
+    private float _horizontalInput;
+    private float _forwardInput;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //this is where we get player input
+        _horizontalInput = Input.GetAxis("Horizontal");
+        _forwardInput = Input.GetAxis("Vertical");
         
+       //We move the vehicle forward  
+       transform.Translate(Vector3.forward * (Time.deltaTime * speed*_forwardInput));
+       //we turn the vehicle
+       transform.Rotate(Vector3.up,Time.deltaTime * turnSpeed*_horizontalInput);
     }
 }
