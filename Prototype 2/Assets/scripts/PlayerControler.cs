9d2daab598c5b4a2fa2208f744f36523c52362c6
Racheal <10573008@my.uvu.edu>
2020-09-30 00:42:58 -0600

    working on cwc2

---
+++ Prototype 2/Assets/scripts/PlayerControler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public float horizantalInput;
+    public float speed = 10.0f;
+    public float xrange = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -xrange)  
+        {
+            transform.position=new Vector3(-xrange,transform.position.y,transform.position.x);
+        }
+        if (transform.position.x >xrange)  
+        {
+            transform.position=new Vector3(xrange,transform.position.y,transform.position.x);
+        }
+        horizantalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * (horizantalInput * Time.deltaTime * speed));
+    }
+}
