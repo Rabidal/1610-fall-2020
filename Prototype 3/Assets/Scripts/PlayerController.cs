10efea76836f44d1349993e09d179acfdb03aace
Racheal <10573008@my.uvu.edu>
2020-10-09 20:03:39 -0600

    3.0 cont.

--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -1,24 +1,34 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody _playerRb;
-    
+    public float jumpForce = 10;
+    public float gravityModifier;
+    public bool isOnGround = true;
     // Start is called before the first frame update
     void Start()
     {
         _playerRb = GetComponent<Rigidbody>();
-        
+        Physics.gravity *= gravityModifier;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space)&& isOnGround)
         {
-            _playerRb.AddForce(Vector3.up*10,ForceMode.Impulse);
+            _playerRb.AddForce(Vector3.up*jumpForce,ForceMode.Impulse);
+            isOnGround = false;
         }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        isOnGround = true;
     }
 }
