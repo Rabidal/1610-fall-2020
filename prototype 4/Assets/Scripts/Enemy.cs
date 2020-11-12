a177974ec3ec827e371fc38f4ac37f7982a91a75
Racheal <10573008@my.uvu.edu>
2020-11-11 20:17:27 -0700

    4.0

---
+++ prototype 4/Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 3.0f;
+    private GameObject _player;
+private Rigidbody _enemyRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _enemyRb = GetComponent<Rigidbody>();
+        _player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lookDirections = (_player.transform.position - transform.position).normalized;
+        _enemyRb.AddForce(lookDirections*speed);
+    }
+}
