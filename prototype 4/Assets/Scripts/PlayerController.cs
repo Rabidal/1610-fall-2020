7d98692bef6274e30ba430e8c6693b669bd4f365
Racheal <10573008@my.uvu.edu>
2020-11-19 20:44:23 -0700

    4.0

--- prototype 4/Assets/Scripts/PlayerController.cs
+++ prototype 4/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,6 +8,7 @@ public class PlayerController : MonoBehaviour
     private Rigidbody _playerRb;
     private GameObject _focalPoint;
     public float speed = 5.0f;
+    public bool hasPowerup = false;
     
     // Start is called before the first frame update
     void Start()
@@ -22,4 +24,22 @@ public class PlayerController : MonoBehaviour
         
         _playerRb.AddForce(_focalPoint.transform.forward * (forwardInput * speed));
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy")&& hasPowerup)
+        {
+            Debug.Log("Collided with: "+collision.gameObject.name+"with powerup set to"+hasPowerup);
+        }
+    }
 }
+
