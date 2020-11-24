7707d41c17ad8f1c391042f62483b95d61bbc854
Racheal <10573008@my.uvu.edu>
2020-11-23 19:35:02 -0700

    4.0

--- prototype 4/Assets/Scripts/PlayerController.cs
+++ prototype 4/Assets/Scripts/PlayerController.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using Vector3 = System.Numerics.Vector3;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody _playerRb;
     private GameObject _focalPoint;
+    private float _powerUpStrength = 15;
     public float speed = 5.0f;
     public bool hasPowerup = false;
     
@@ -31,13 +34,23 @@ public class PlayerController : MonoBehaviour
         {
             hasPowerup = true;
             Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
         }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yeild return new WaitForSeconds(7);
+        hasPowerup = false; 
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Enemy")&& hasPowerup)
         {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            UnityEngine.Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+            enemyRigidbody.AddForce(awayFromPlayer*_powerUpStrength,ForceMode.Impulse);
             Debug.Log("Collided with: "+collision.gameObject.name+"with powerup set to"+hasPowerup);
         }
     }
