933880d364f79e5d1e1628399700aa62a12ca271
Racheal <10573008@my.uvu.edu>
2020-12-11 22:59:53 -0700

    final game

---
+++ Final game 2020/Assets/Free Pixel Space Platform Pack/Astronaut/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public int playerSpeed = 10;
+    public bool facingRight = true;
+    public Rigidbody2D rigidbody2d;
+    public float moveX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+    }
+     void PlayerMove()
+    {
+       //CONTROLS
+       moveX = Input.GetAxis("Horizontal");
+       if (Input.GetButtonDown("Jump"))
+       {
+           Jump();
+       }
+       //ANIMATIONS
+       //PLAYER DIRECTION
+       if (moveX < 0.0f && facingRight== false)
+       {
+           FlipPlayer();
+       }
+       else if (moveX>0.0f&& facingRight==true)
+       {
+           FlipPlayer();
+       }
+       //PHYSICS
+       gameObject.GetComponent<Rigidbody2D>().velocity=new Vector2(moveX*playerSpeed,gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+     void Jump()
+     {
+         //JUMPING CODE 
+         if (Input.GetKeyDown(KeyCode.Space))
+         {
+             float jumpVelocity = 30f;
+             rigidbody2d.velocity = Vector2.up * jumpVelocity;
+         }
+     }
+
+     void FlipPlayer()
+     {
+         facingRight = !facingRight;
+         Vector2 localScale = gameObject.transform.localScale;
+         localScale.x *= -1;
+         transform.localScale = localScale;
+     }
+}
---
+++ Final game 2020/Assets/Free Pixel Space Platform Pack/Astronaut/Scripts/PlayerHealth.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PlayerHealth : MonoBehaviour
+{
+    public bool hasDied;
+    public int health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hasDied = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.transform.position.y<-8)
+        {
+            hasDied=true;
+        }
+
+        if (hasDied==true)
+        {
+            StartCoroutine(Die());
+        }
+
+        IEnumerator Die()
+        {
+            SceneManager.LoadScene("First");
+            yield return null;
+
+        }
+    }
+}
